d7021615e8eb0303052c2036e141722be9c88bfe
Intel-out-side <so.eng.eng.1rou@gmail.com>
2019-04-18 14:51:56 +0900

    2019/04/18

--- runned_on_ArdMicro/runned_on_ArdMicro.ino
+++ runned_on_ArdMicro/runned_on_ArdMicro.ino
@@ -15,7 +15,7 @@ int move_y; //relative distance to move in Y-axis
 float C = 80; // constant to normalize
 float acc_x, acc_y, acc_z; //raw data from the sensor
 float acc_angX, acc_angY, acc_angZ; //calculated angles 
-unsigned long dt;
+unsigned long dt; //duration for a single loop
 int s; // number of times the user pushed the switch 1
 int f; // number of times the user pushed the switch 2
 
@@ -52,6 +52,7 @@ void setup() {
   Wire.write(0x05);
   Wire.endTransmission();
 
+  //Initialize the all LED
   pinMode(IndicateMode, OUTPUT);
   pinMode(IndicateLow, OUTPUT);
   pinMode(IndicateMid, OUTPUT);
@@ -86,12 +87,12 @@ void loop() {
   acc_y = ayRaw / 16384.0;
   acc_z = azRaw / 16384.0;
 
-  acc_angX = atan2(acc_y, acc_x) * 360 / 2.0 / PI;
+  acc_angX = atan2(acc_y, acc_x) * 360 / 2.0 / PI; //calculatete the tilt angle about X-axis
   Serial.print(acc_angX); Serial.print(",");
-  acc_angY = atan2(acc_z, acc_x) * 360 / 2.0 / PI;
+  acc_angY = atan2(acc_z, acc_x) * 360 / 2.0 / PI; //calculate the tilt angle about Y-axis
   Serial.println(acc_angY);
 
-  if (digitalRead(4)==LOW){
+  if (digitalRead(4)==LOW){ //if the left switch is pushed then s+=1
     s+=1;
     if (s>2) s = 0;
     delay(100);
@@ -99,7 +100,7 @@ void loop() {
     twinkle(1);
     }
 
-  if (digitalRead(5)==LOW){
+  if (digitalRead(5)==LOW){ //if the right switch is pushed then f+=1
     f += 1;
     if (f>2) f=0;
     delay(100);
@@ -108,7 +109,7 @@ void loop() {
     }
   distinguisher();
   sensitivity_changer();
-  Mouse.move(move_x, move_y, 0);
+  Mouse.move(move_x, move_y, 0); // Move the cursor according to the tilt angles
   dt = (double) (millis() - t)/1000.0;
 }
 
