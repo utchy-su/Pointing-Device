ff805fad98ba1da65b82a9dbb63995eb1dbe336d
Intel-out-side <so.eng.eng.1rou@gmail.com>
2020-06-06 21:49:07 +0900

    untracked some folders

--- Ver1_Device/Header.h
+++
@@ -1,7 +0,0 @@
-int16_t axRaw, ayRaw, azRaw, gxRaw, gyRaw, gzRaw;
-const float kx=0.2; //gain for x
-const float ky=0.2; //gain for y
-int move_x; //relative distance to move along X-axis
-int move_y; //relative distance to move along Y-axis
-float acc_x, acc_y, acc_z;
-float acc_angX, acc_angY;
